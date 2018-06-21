8c8fbb289b5a02969bf21af361ebe9a867941bca
Daniel <daniel@tohka.us>
2018-06-21 22:04:54 +0300

    Change

--- lineage.mk
+++ lineage.mk
@@ -1,8 +1,8 @@
+# Inherit framework
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/actions/gs702a/full_gs702a.mk)
-
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit GSM common stuff
 PRODUCT_COPY_FILES += \
@@ -14,6 +14,12 @@ PRODUCT_BRAND := Android
 PRODUCT_DEVICE := gs702a
 PRODUCT_MODEL := Overmax NewBase 2
 PRODUCT_MANUFACTURER := overmax
+PRODUCT_GMS_CLIENTID_BASE := android-overmax
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_gs702a-userdebug 8.1 eng.disyer test-keys" \
+    PRODUCT_NAME=gs702a \
+    TARGET_DEVICE=NewBase2
 
+BUILD_FINGERPRINT := Android/full_gs702a/gs702a:4.1.1/JRO03C/eng.zhouzf.20130720.141654:userdebug/test-keys
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
